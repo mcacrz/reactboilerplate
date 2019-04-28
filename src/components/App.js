267bad59f18934cecf3bbfdb462c7d670a44d12c
Marcelo Cruz <mca.crz@gmail.com>
2019-04-28 13:16:47 -0300

    first commit from project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <p>Hello World!!!</p>
+        <p>This is a BoilerPlate React, use to create a new React Project</p>
+      </div>
+    );
+  }
+}
+
+export default App
